3629a61f3e5bd69dd71427fa113fa61380da3249
Tsimur Mikula <mikulatsimur@yandex.ru>
2023-02-02 00:09:58 +0300

    feat: Accourdion add in plants#3

--- plants/js/script.js
+++ plants/js/script.js
@@ -22,3 +22,29 @@
 console.log('1. Вёрстка валидная +10\n2. Вёрстка семантическая +20\n3. Вёрстка соответствует макету +48\n4. Требования к css + 12\n5. Интерактивность, реализуемая через css +20\nИтоговая оценка: 100 баллов.');
 console.log('1. Вёрстка соответствует макету. Ширина экрана 768px +24\n2. Вёрстка соответствует макету. Ширина экрана 380px +24\n3. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки. Весь контент страницы при этом сохраняется: не обрезается и не удаляется +15\n4. На ширине экрана 380рх и меньше реализовано адаптивное меню +22 (Допускается появление адаптивного меня на ширине более 380, но не допускается на ширине более 770px)');
 
+(function () {
+    const pricesAccourdion= document.querySelectorAll('.price__accourdion_title');
+    
+    pricesAccourdion.forEach(priceAccourdion => {
+        priceAccourdion.addEventListener('click', () => {
+            if (priceAccourdion.classList.contains('is-open')) {
+                priceAccourdion.classList.remove('is-open');
+            } else {
+                const pricesAccourdionWithIsOpen = document.querySelectorAll('.is-open');
+                pricesAccourdionWithIsOpen.forEach(priceAccourdionWithIsOpen => {
+                    priceAccourdionWithIsOpen.classList.remove('is-open');
+                });
+                priceAccourdion.classList.add('is-open');
+            } 
+        }); 
+    });
+}());
+
+(function () {
+    const pricesAccourdionIcon = document.querySelectorAll('.price__accourdion_title-icon')
+   pricesAccourdionIcon.forEach(priceAccourdionIcon => {
+    priceAccourdionIcon.addEventListener('click', () => {
+    pricesAccourdionIcon.classList.toggle('price__accourdion_title-iconTransform');
+    })
+    })
+}());
